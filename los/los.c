22fd55483d995734e7c7e0b348e1000382ac689c
Falko Goettsch <mr.falko@gmail.com>
2018-11-04 20:08:27 -0800

    removed commented out logger calls

--- los/los.c
+++ los/los.c
@@ -31,7 +31,6 @@ static bool braise(level *lvl, int a_x, int a_y, int b_x, int b_y, checker_func 
     int *stepper, *step;
     int *bumper, *bump;
 
-    //logger("=== Initialized ===\na=(%d,%d) b=(%d,%d) dxy=(%d + %d,%d + %d)\n",a_x,a_y,b_x,b_y,dx,x_increment,dy,y_increment);
 
     // set up which values will be modified based on
     // the slope of the line
@@ -66,7 +65,6 @@ static bool braise(level *lvl, int a_x, int a_y, int b_x, int b_y, checker_func 
     int drain = *run;
 
     while (true) {
-        //logger("Step #%d: (%d,%d) step: %d + %d bump: %d + %d error: %d + %d - %d\n",i+1,x,y,*stepper,*step,*bumper,*bump,error,increment,drain);
         // handle horizontal and vertical exception cases
         // (This could be way more optimized)
         if (dx == 0) {
@@ -94,13 +92,11 @@ static bool braise(level *lvl, int a_x, int a_y, int b_x, int b_y, checker_func 
             // We made it. Doesn't matter if the final square
             // was inaccessible, because most of the time it'll
             // be occupied by something.
-            //logger("Made it to (%d,%d)\n", x, y);
             return true;
         }
 
         /* check for valid space here */
         if (! checker(lvl, x, y)) {
-            //logger("Failed checker() on step #%d out of %d at (%d,%d)\n", i + 1, *run, x, y);
             return false;
         }
     }
